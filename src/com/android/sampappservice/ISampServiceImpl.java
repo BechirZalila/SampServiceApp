3a33a98bb97a40433ad821e2dc911e3b17e5ee41
Bechir Zalila <bechir.zalila@gmail.com>
2019-07-09 15:35:35 +0100

    * Display log

--- src/com/android/sampappservice/ISampServiceImpl.java
+++ src/com/android/sampappservice/ISampServiceImpl.java
@@ -1,18 +1,24 @@
 package com.android.sampappservice;
  
+import android.util.Log;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.RemoteException;
 import com.android.sampservice.*;
  
 class ISampServiceImpl extends ISampService.Stub {
- 
+  public static String TAG = "ISampServiceImpl";
+
   public int add(int a,int b) {
-    return a+b ;                           
+	  int c = a + b;
+	  Log.d (TAG, "Addition: " + a + " + " + b + " = " + c);
+	  return c;                           
   }
  
   public int sub(int a,int b) {
-    return a-b;                           
+	  int c = a - b;
+	  Log.d (TAG, "Substraction: " + a + " - " + b + " = " + c);
+	  return c;
   }
 }
 
